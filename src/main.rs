a854a707d6e4dba3da10b8bbde1e10e3a5214b6d
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2024-10-30 22:38:52 +0800

    添加欢迎界面

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use simple_logger::SimpleLogger;
 use sr_plot_rs::plot::Plot;
+use std::fmt::Write;
+use unicode_width::UnicodeWidthStr;
+
+const WELCOME: &str = r#"
+请使用「管理员身份」运行此程序
+自动对话需要保持窗口在前台
+若游戏为「窗口化」，请确保鼠标位置在游戏窗口内
+"#;
 
 fn main() {
     SimpleLogger::new()
@@ -7,6 +15,8 @@ fn main() {
         .with_module_level("xcap", log::LevelFilter::Off)
         .init()
         .unwrap();
+
+    println!("{}", hr(WELCOME));
 
     let image_files = vec![
         ("start.png", include_bytes!("../assets/start.png").to_vec()),
@@ -26,4 +36,28 @@ fn main() {
     );
     let plot = Plot::new("崩坏：星穹铁道".to_string(), select_image, image_files);
     plot.run();
+}
+
+fn hr(title: &str) -> String {
+    let mut output = String::new();
+    // 左右留下长度为 10 的空格
+    let max_length = title.split("\n").map(|line| line.width()).max().unwrap() + 22;
+
+    writeln!(&mut output, "+{}+", "-".repeat(max_length - 2)).unwrap();
+    for line in title.split("\n") {
+        let length = line.width();
+        let left_padding = (max_length - length - 2) / 2;
+        let right_padding = max_length - length - left_padding - 2;
+        writeln!(
+            &mut output,
+            "|{}{}{}|",
+            " ".repeat(left_padding),
+            line,
+            " ".repeat(right_padding)
+        )
+        .unwrap();
+    }
+    writeln!(&mut output, "+{}+", "-".repeat(max_length - 2)).unwrap();
+
+    output
 }
