fe273f609266922752cd35d4f1e5f35fdd7e9fb9
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2024-11-01 15:23:34 +0800

    小重构 && 修正了部分代码

--- src/plot.rs
+++ src/plot.rs
@@ -3,7 +3,7 @@ use std::{
     time::{Duration, Instant},
 };
 
-use crate::{automation::Automation, error::SrPlotResult, utils::get_window};
+use crate::{automation::Automation, error::SrPlotResult, utils::get_window, xcap::Window};
 
 use colored::Colorize;
 
@@ -28,11 +28,7 @@ pub struct Plot {
 }
 
 impl Plot {
-    pub fn new(
-        game_title_name: String,
-        select_img: (&'static str, Vec<u8>),
-        start_img: Vec<(&'static str, Vec<u8>)>,
-    ) -> Self {
+    pub fn new(game_title_name: String, select_img: ImageFile, start_img: Vec<ImageFile>) -> Self {
         Self {
             auto: Automation::new(&game_title_name),
             select_img,
@@ -53,56 +49,67 @@ impl Plot {
     }
 
     fn check_game_status(&mut self) -> SrPlotResult<()> {
-        let time = Instant::now();
-        if let Some(window) = get_window(&self.game_title_name) {
-            self.is_window_exist = true;
-            if window.is_active() {
-                handle_status_change(&mut self.is_window_active, true, || {
-                    log::info!("{}", "游戏窗口已激活！正在执行中……".green().bold())
-                });
-
-                self.auto.take_screenshot(START_IMAGE_CROP.into())?;
-
-                // 缩放大小，匹配窗口分辨率
-                let scale_factor = window.width() as f64 / 1920.0;
-                let scale_range = (scale_factor < 1.0).then(|| {
-                    (
-                        ((scale_factor - 0.05) * 10.0).round() / 10.0,
-                        ((scale_factor + 0.05) * 10.0).round() / 10.0,
-                    )
-                });
-
-                for img in &self.start_img {
-                    let result = self.auto.find_element((img.0, &img.1), 0.9, scale_range)?;
-                    if result.is_some() {
-                        let select_img = &self.select_img;
-                        // 查找是否有选项要点
-                        self.auto.take_screenshot(SELECT_IMAGE_CROP.into())?;
-                        match self.auto.find_element(
-                            (select_img.0, &select_img.1),
-                            0.88, // 遇到过 0.89 匹配不上，所以降低 threshold
-                            scale_range,
-                        )? {
-                            // 有选项就点击选项
-                            Some(coordinate) => self.auto.click_with_coordinate(coordinate)?,
-                            // 没选项就随便点
-                            None => self.auto.click()?,
-                        }
-                        break;
-                    }
+        match get_window(&self.game_title_name) {
+            Some(window) => {
+                self.is_window_exist = true;
+                if window.is_active() {
+                    handle_status_change(&mut self.is_window_active, true, || {
+                        log::info!("{}", "游戏窗口已激活！正在执行中……".green().bold())
+                    });
+                    self.autoplot(&window)?;
+                } else {
+                    handle_status_change(&mut self.is_window_active, false, || {
+                        log::warn!("{}", "检测到游戏窗口未激活，停止执行！".blue().bold())
+                    });
                 }
-                log::debug!("执行完毕！总耗时：{}ms", time.elapsed().as_millis());
-            } else {
-                handle_status_change(&mut self.is_window_active, false, || {
-                    log::warn!("{}", "检测到游戏窗口未激活，停止执行！".blue().bold())
+            }
+            None => {
+                self.is_window_active = false;
+                handle_status_change(&mut self.is_window_exist, false, || {
+                    log::warn!("{}", "未检测到游戏窗口，等待游戏启动……".cyan().bold());
                 });
             }
-        } else {
-            self.is_window_active = false;
-            handle_status_change(&mut self.is_window_exist, false, || {
-                log::warn!("{}", "未检测到游戏窗口，等待游戏启动……".cyan().bold())
-            });
         }
+        Ok(())
+    }
+
+    fn autoplot(&mut self, window: &Window) -> SrPlotResult<()> {
+        let time = Instant::now();
+
+        self.auto.take_screenshot(START_IMAGE_CROP.into())?;
+
+        // 缩放大小，匹配窗口分辨率
+        let scale_factor = window.width() as f64 / 1920.0;
+        let scale_range = if scale_factor < 1.0 {
+            Some((
+                ((scale_factor - 0.05) * 10.0).round() / 10.0,
+                ((scale_factor + 0.05) * 10.0).round() / 10.0,
+            ))
+        } else {
+            None
+        };
+
+        for img in &self.start_img {
+            if self
+                .auto
+                .find_element((img.0, &img.1), 0.9, scale_range)?
+                .is_some()
+            {
+                self.auto.take_screenshot(SELECT_IMAGE_CROP.into())?;
+                match self.auto.find_element(
+                    (self.select_img.0, &self.select_img.1),
+                    0.88, // 遇到过 0.89 匹配不上，所以降低 threshold
+                    scale_range,
+                )? {
+                    // 有选项就点击选项
+                    Some(coordinate) => self.auto.click_with_coordinate(coordinate)?,
+                    // 没选项就随便点
+                    None => self.auto.click()?,
+                }
+                break;
+            }
+        }
+        log::debug!("执行完毕！总耗时：{}ms", time.elapsed().as_millis());
         Ok(())
     }
 }
