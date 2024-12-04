ad557f7b5adf283928b72903bce2e0578d7943d2
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2024-12-04 23:52:42 +0800

    fix: 删除了可变静态变量的可变引用（在 Rust2024 环境中会报警告）

--- src/input.rs
+++ src/input.rs
@@ -1,26 +1,30 @@
-use std::{sync::OnceLock, thread, time::Duration};
+use std::{
+    sync::{Mutex, OnceLock},
+    thread,
+    time::Duration,
+};
 
 use enigo::{Button, Direction, Enigo, Mouse, Settings};
 
 use crate::error::SrPlotResult;
 
-static mut ENIGO_INS: OnceLock<Enigo> = OnceLock::new();
+static ENIGO_INS: OnceLock<Mutex<Enigo>> = OnceLock::new();
 
-fn get_enigo<'a>() -> &'a mut Enigo {
-    unsafe {
-        ENIGO_INS.get_or_init(|| Enigo::new(&Settings::default()).unwrap());
-        ENIGO_INS.get_mut().unwrap()
-    }
+fn get_enigo<'a>() -> std::sync::MutexGuard<'a, Enigo> {
+    let enigo_locker =
+        ENIGO_INS.get_or_init(|| Mutex::new(Enigo::new(&Settings::default()).unwrap()));
+    enigo_locker.lock().unwrap()
 }
 
 pub struct Input;
 
 impl Input {
     pub fn click() -> SrPlotResult<()> {
-        get_enigo().button(Button::Left, Direction::Press)?;
+        let mut enigo = get_enigo();
+        enigo.button(Button::Left, Direction::Press)?;
         // 经测试，正常的间隔大概在 75~100ms 左右
         thread::sleep(Duration::from_millis(50));
-        get_enigo().button(Button::Left, Direction::Release)?;
+        enigo.button(Button::Left, Direction::Release)?;
         Ok(())
     }
 
